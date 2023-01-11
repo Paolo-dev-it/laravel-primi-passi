af078aad01f026781f475e6d97c80c3d6487f9af
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-01-11 14:13:37 +0100

    Created first title

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primi passi Laravel</title>
+</head>
+
+<body>
+    <h1>Primi passi con Laravel</h1>
+</body>
+
+</html>
